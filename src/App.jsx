230cf56841bbd1b22db94fec3137ef815dd1e1ac
Your satyam <your-email@example.com>
2024-09-09 11:41:55 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,17 @@
-import { useState } from 'react'
-import './App.css'
+
+
 import Header from './componantes/Header'
-// import Footer from './componantes/Footer'
+ 
 import Home from './componantes/Home'
-// import logIn from './componantes/logIn'
-// import Chatbott from './componantes/Chatbott'
-// import Sidebar from './componantes/Main/Sidebar'
-// import Body from './componantes/Main/Body'
+ 
 
 
  
-// import { createBrowserRouter } from 'react-router-dom'
  
 
 
 function App() {
-  //  const [count,setcount]=useState(0)
+   
 
   return (
     <>
@@ -23,8 +19,8 @@ function App() {
     <Header/>
     {/* <Footer/> */}
     <Home/>
-    <Chatbott/>
-     <h1 className='bg-green-400  p-4'> Hii i am going to learn react Router</h1>
+   
+    
     </>
   )
 }
--- src/componantes/Home.jsx
+++ src/componantes/Home.jsx
@@ -1,48 +1,36 @@
-import React  from "react";
-import {NavLink, useNavigate} from 'react-router-dom';
-// import Chatbott from "./Chatbott";
-// import Body from "./main/Bodyy";
-import './Home.css'
-export default function Home() {
-    const navigate= useNavigate()
-    const GoChat = ()=>{
-        navigate("/Bodyy");
-    }
-    return (
-        <div className="mr-0 w-full ">
-            <div className="relative overflow-hidden text-black rounded-lg sm:mx-16 mx-2 sm:py-24">
-                <div className="relative z-10 max-w-screen-xl px-4  pb-20 pt-10 sm:py-24 mx-auto sm:px-6 lg:px-8">
-                    <div className="max-w-xl sm:mt-1   mt-80 space-y-8 text-center sm:text-right sm:ml-auto ">
-                        <h2 className="text-4xl font-bold sm:text-5xl">s
-                            Come Here
-                            <span className="hidden sm:block text-4xl">Let's Get Anlyse</span>
-                        </h2>
+import { NavLink } from "react-router-dom";
 
-                        <NavLink  
-                            className=" button inline-flex   text-white items-center px-6 py-3 font-medium bg-orange-700 focus:ring-orange-400 rounded-lg hover:opacity-75"
-                            to="/Bodyy"
-                        >
-                            <svg
-                                fill="white"
-                                width="24"
-                                height="24"
-                                xmlns="http://www.w3.org/2000/svg"
-                                fillRule="evenodd"
-                                clipRule="evenodd"
-                            >
-                                <path d="M16.172 11l-5.364-5.364 1.414-1.414L20 12l-7.778 7.778-1.414-1.414L16.172 13H4v-2z" />
-                            </svg>
-                            &nbsp; Let's Chat
-                            
-                        </NavLink>
-                    </div>
-                </div>
- 
-            </div>
- 
- 
+import "./Home.css";
+export default function Home() {
+  return (
+    <div className="mr-0 w-full ">
+      <div className="relative overflow-hidden text-black rounded-lg sm:mx-16 mx-2 sm:py-24">
+        <div className="relative z-10 max-w-screen-xl px-4  pb-20 pt-10 sm:py-24 mx-auto sm:px-6 lg:px-8">
+          <div className="max-w-xl sm:mt-1   mt-80 space-y-8 text-center sm:text-right sm:ml-auto ">
+            <h2 className="text-4xl font-bold sm:text-5xl">
+              Come Here
+              <span className="hidden sm:block text-4xl">Let Get Anlyse</span>
+            </h2>
+
+            <NavLink
+              className=" button inline-flex   text-white items-center px-6 py-3 font-medium bg-orange-700 focus:ring-orange-400 rounded-lg hover:opacity-75"
+              to="/Bodyy"
+            >
+              <svg
+                fill="white"
+                width="24"
+                height="24"
+                xmlns="http://www.w3.org/2000/svg"
+                fillRule="evenodd"
+                clipRule="evenodd"
+              >
+                <path d="M16.172 11l-5.364-5.364 1.414-1.414L20 12l-7.778 7.778-1.414-1.414L16.172 13H4v-2z" />
+              </svg>
+              &nbsp; Let Chat
+            </NavLink>
+          </div>
         </div>
-        
-    );
-    
+      </div>
+    </div>
+  );
 }
